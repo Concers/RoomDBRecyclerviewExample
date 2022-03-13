040ef73f1782ac23d174c1024a2bafb74e19b42a
Uğur Erol <info@geceugurerol.com>
2022-03-13 21:38:47 +0300

    First commit

---
+++ app/src/main/java/com/ovologos/roomdbrecyclerviewexample/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.ovologos.roomdbrecyclerviewexample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
